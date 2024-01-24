bb36669895e7b000214ec4636d3a543466cf815b
Pickle-Morty <avatarbliz@gmail.com>
2024-01-24 13:30:12 +0600

    fix conflict

--- app/ru/(pages)/layout.js
+++ app/ru/(pages)/layout.js
@@ -4,7 +4,6 @@ import Header from "../components/Header/Header";
 import { getDictionary } from "../../../get-dictionary";
 
 
-<<<<<<< HEAD
 
 export async function generateMetadata({ params: { lang } }) {
     return {
@@ -12,15 +11,6 @@ export async function generateMetadata({ params: { lang } }) {
         description: "Одон Строй | Строительство зданий из сборных конструкций ✅Лицензированная компания✅Гарантия на обслуживание⚡️Получите консультацию на сайте",
     };
 }
-=======
-export async function generateMetadata({ params: { lang } }) {
-    return {
-        title: "Одон Строй" | "Здания из сборных конструкций Бишкек",
-        description: "Одон Строй | Строительство зданий из сборных конструкций ✅Лицензированная компания✅Гарантия на обслуживание⚡️Получите консультацию на сайте",
-    };
-}
-
->>>>>>> 76f498ae77a2987557962db678cb45d44460f02b
 
 export default async function RootLayout({ children, params }) {
     return (
